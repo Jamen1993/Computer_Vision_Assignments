5a60472a7bca17e9f1f9466a2ce507335f6ba2ea
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-09 01:00:29 +0200

    Finished task 1.8

--- Assignment_1/assignment_1.m
+++ Assignment_1/assignment_1.m
@@ -1,11 +1,6 @@
 % Testskript für Assignment 1
 close all;
 
-img = imread('Test_Image.jpg');
-gray = rgb_to_gray(img);
-merkmale = harris_detektor_E(gray, 'min_dist', 10, 'N', 30, 'tile_size', [200, 100]);
-
-% Ausgabe der Parameter
-min_dist = merkmale{1}
-tile_size = merkmale{2}
-N = merkmale{3}
+Cake = cake(50);
+colormap('gray');
+imagesc(Cake)
---
+++ Assignment_1/cake.m
@@ -0,0 +1,15 @@
+function Cake = cake(min_dist)
+    % Die Funktion cake erstellt eine "Kuchenmatrix", die eine kreisförmige
+    % Anordnung von Nullen beinhaltet und den Rest der Matrix mit Einsen
+    % auffüllt. Damit können, ausgehend vom stärksten Merkmal, andere Punkte
+    % unterdrückt werden, die den Mindestabstand hierzu nicht einhalten.
+
+    % Matrix generieren in der jedes Element seinen Abstand vom Mittelpunkt entsprechend euklidischer Norm repräsentiert.
+    index = -min_dist:min_dist;
+    [X, Y] = meshgrid(index, index);
+    distances = sqrt(X .^ 2 + Y .^ 2);
+    % Elemente mit Abstand kleiner gleich min_dist zu Null setzen
+    Cake = ones(min_dist * 2 + 1) .* (distances > min_dist);
+    % Konvertierung in boolsche Maske
+    Cake = logical(Cake);
+end
