155f79a668ecb7891eba85571f71e7728c821540
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-09 00:02:29 +0200

    Finished task 1.5

--- Assignment_1/assignment_1.m
+++ Assignment_1/assignment_1.m
@@ -3,21 +3,13 @@ close all;
 
 img = imread('Test_Image.jpg');
 gray = rgb_to_gray(img);
-merkmale = harris_detektor_B(gray, 'segment_length', 9, 'k', 0.06);
-Ix = merkmale{1};
-Iy = merkmale{2};
+merkmale = harris_detektor_C(gray, 'segment_length', 9, 'k', 0.06);
+H = merkmale{1};
+corners = merkmale{2};
+merkmale = merkmale{3};
 
-figure('name', 'Sobelfilter');
-subplot(1,2,1), imshow(Ix)
-subplot(1,2,2), imshow(Iy)
+figure();
+subplot(1,2,1), imshow(H)
+subplot(1,2,2), imshow(corners)
 
-G11 = merkmale{4};
-G22 = merkmale{5};
-G12 = merkmale{6};
-
-figure('name', 'Harrisdetektor');
-subplot(1,3,1), imshow(G11)
-subplot(1,3,2), imshow(G22)
-subplot(1,3,3), imshow(G12)
-
-w = merkmale{3}
+merkmale(:,min(5,size(merkmale,2)));
