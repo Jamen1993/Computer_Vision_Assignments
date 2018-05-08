69355ff35a549f0798dda222a384fdc92f839d95
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-09 00:34:01 +0200

    Finished task 1.7

--- Assignment_1/assignment_1.m
+++ Assignment_1/assignment_1.m
@@ -3,4 +3,9 @@ close all;
 
 img = imread('Test_Image.jpg');
 gray = rgb_to_gray(img);
-merkmale = harris_detektor_D(gray, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
+merkmale = harris_detektor_E(gray, 'min_dist', 10, 'N', 30, 'tile_size', [200, 100]);
+
+% Ausgabe der Parameter
+min_dist = merkmale{1}
+tile_size = merkmale{2}
+N = merkmale{3}
