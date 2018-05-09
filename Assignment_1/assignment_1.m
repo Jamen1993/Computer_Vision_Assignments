73e741e61f8ad82645bc02f45fc1f7ca4d2aaa56
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-09 13:04:31 +0200

    * Cleanup * More cleanup in code required

--- Assignment_1/assignment_1.m
+++ Assignment_1/assignment_1.m
@@ -3,4 +3,4 @@ close all;
 
 img = imread('Test_Image.jpg');
 gray = rgb_to_gray(img);
-merkmale = harris_detektor_H(gray, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
+merkmale = harris_detektor(gray, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
