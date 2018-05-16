c63791e35ba2e9360dfccbd7127251f9af8c9664
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-16 23:57:29 +0200

    Removed second test image; it was not that helpful because of its complexity and size

--- Assignment_1/assignment_1.m
+++ Assignment_1/assignment_1.m
@@ -1,12 +1,9 @@
 % Testskript für Assignment 1
 close all;
 
-img_1 = imread('Test_Image_1.jpg');
-img_2 = imread('Test_Image_2.jpg');
-gray_1 = rgb_to_gray(img_1);
-gray_2 = rgb_to_gray(img_2);
+img = imread('Test_Image.jpg');
+gray = rgb_to_gray(img);
 tic;
-merkmale_1 = harris_detektor(gray_1, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
-t_1 = toc;
-merkmale_2 = harris_detektor(gray_2, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
-t_2 = toc - t_1;
+merkmale = harris_detektor(gray, 'segment_length', 9, 'k', 0.06, 'do_plot', true);
+t = toc;
+fprintf('Die Laufzeit war %f s\n', t);
