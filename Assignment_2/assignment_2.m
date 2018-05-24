de9bb4e265a3a11bf005eed8926a97bcb6009a33
Simon Diehl <j.simon.diehl@googlemail.com>
2018-05-24 19:37:57 +0200

    * Added testscript for 2. assignment * Copied test images from cody coursework

---
+++ Assignment_2/assignment_2.m
@@ -0,0 +1,14 @@
+% Testscript für Assignment 2
+close all;
+
+Image1 = imread('szeneL.png');
+IGray1 = rgb_to_gray(Image1);
+
+Image2 = imread('szeneR.png');
+IGray2 = rgb_to_gray(Image2);
+
+% Harris-Merkmale berechnen
+Merkmale1 = harris_detektor(IGray1,'segment_length',9,'k',0.05,'min_dist',50,'N',20,'do_plot',false);
+Merkmale2 = harris_detektor(IGray2,'segment_length',9,'k',0.05,'min_dist',50,'N',20,'do_plot',false);
+
+Korrespondenzen = punkt_korrespondenzen(IGray1,IGray2,Merkmale1,Merkmale2,'window_length',25,'min_corr', 0.90,'do_plot',true)
