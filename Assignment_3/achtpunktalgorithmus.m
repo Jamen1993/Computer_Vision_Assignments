7121254d569d0dcad68846828497092d90169e06
Simon Diehl <j.simon.diehl@googlemail.com>
2018-06-11 13:02:00 +0200

    Partial solution for 3.2 -> Essential Matrix

--- Assignment_3/achtpunktalgorithmus.m
+++ Assignment_3/achtpunktalgorithmus.m
@@ -1,5 +1,5 @@
 function EF = achtpunktalgorithmus(Korrespondenzen, K)
-    % Diese Funktion berechnet die Essentielle Matrix oder Fundamentalmatrix mittels 8-Punkt-Algorithmus, je nachdem, ob die Kalibrierungsmatrix 'K' gegeben ist oder nicht.
+    % Diese Funktion berechnet die essenzielle Matrix oder Fundamentalmatrix mittels des 8-Punkt-Algorithmus je nachdem, ob die Kalibrierungsmatrix K gegeben ist oder nicht.
 
     % Standardwert für K setzen
     if nargin == 1
@@ -21,18 +21,32 @@ function EF = achtpunktalgorithmus(Korrespondenzen, K)
         end
     end
 
-    % Punkte für Schätzung der Essentiellen oder Fudamentalmatrix vorbereiten
+    % Punkte für Schätzung der essenziellen oder Fudamentalmatrix vorbereiten
     x1 = punkte_vorbereiten(Korrespondenzen(1:2, :));
     x2 = punkte_vorbereiten(Korrespondenzen(3:4, :));
 
     % Koeffizientenmatrix für vektorisierte Epipolargleichung berechnen
+    % Ich verwende hier eine Schleife, weil es keine Funktion für die Berechnung eines spaltenweise Kroneckerprodukts gibt.
     A = zeros(length(Korrespondenzen), 9);
-
-    for it = 1:length(Korrespondenzen)
-        A(it, :) = kron(x1(:, it), x2(:, it));
+    % for each Spalte
+    for n = 1:length(Korrespondenzen)
+        A(n, :) = kron(x1(:, n), x2(:, n));
     end
-
+    % Singulärwertzerlegung der Koeffizientenmatrix
     [~, ~, V] = svd(A);
 
-    EF = {x1, x2, A, V};
+    %% Schätzung der Matrizen
+    % Die rechte Spalte von V ist das Ergebnis des Minimierungsproblems Gs = argmin (||A * Es||²) über Es mit Es als vektorisierter Form der essentiellen Matrix und Gs als geschätzter, essenzieller Matrix in vektorisierter Form.
+    % Das so gewonnene G ist in der Regel jedoch keine gültige essenzielle Matrix und muss daher auf die Menge aller gültigen essenziellen Matrizen projiziert werden. Zur Lösung des dafür benötigten Minimierungsproblems E = argmin(||E - G||F²) über E, kann wieder die Singulärwertzerlegung herangezogen werden.
+
+    % Gs aus V extrahieren
+    Gs = V(:, end);
+    % Gs devektorisieren
+    G = reshape(Gs, [3, 3]);
+    % Singulärwertzerlegung von G durchführen
+    [Ug, ~, Vg] = svd(G);
+    % Projektion von G auf den Raum aller essenziellen Matrizen
+    E = Ug * diag([1, 1, 0]) * Vg';
+
+    EF = E;
 end
